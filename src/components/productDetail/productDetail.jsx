cd09d717d0b64beb9b28b62e1dcf97893253c267
Tulio Salvatierra <salvacorp@gmail.com>
2023-08-06 22:06:08 -0500

    Fixed some routing issues within router

--- src/components/productDetail/productDetail.jsx
+++ src/components/productDetail/productDetail.jsx
@@ -5,10 +5,23 @@ import { Grid, Stack } from "@mui/material";
 import { Container } from "reactstrap";
 import { useParams } from "react-router";
 import Iphone from "../../assets/images/iphone.jpeg";
+import { API } from "../../utils/httpsClient";
 
 const ProductDetail = () => {
   const { id } = useParams();
-  console.log(id);
+  const details = async () => {
+    fetch(API + "/products/" + id)
+      .then((res) => res.json())
+      .then((details) => details);
+    console.log(details);
+  };
+
+  const description = async () => {
+    fetch(API + "/products/" + id + "/description")
+      .then((res) => res.json())
+      .then((description) => description);
+    console.log(description);
+  };
 
   return (
     <Container>
@@ -19,7 +32,7 @@ const ProductDetail = () => {
           </Grid>
           <Stack xs={4} item className="pdes">
             <p>Location</p>
-            <h1>Title</h1>
+            <h1>{details.title}</h1>
             <h1>price</h1>
             <Button>Comprar</Button>
           </Stack>
@@ -27,6 +40,7 @@ const ProductDetail = () => {
           <Stack item className="pdeet">
             <h1>title </h1>
             <p>
+              {description.toString()}
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Suspendisse pulvinar tellus eu justo porta dignissim. Aenean vel
               quam at lacus ultrices accumsan et non nunc. Cras luctus libero
