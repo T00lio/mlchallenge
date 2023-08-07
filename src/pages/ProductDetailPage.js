ff522f652342637def4a3f528039aeadf11ea2f6
Tulio Salvatierra <salvacorp@gmail.com>
2023-08-06 21:28:35 -0500

    Added comp to pages

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,12 +1,10 @@
 import ProductDetail from "../components/productDetail";
 import Breadcrumbs from "../components/breadcrumb";
-import { useLocation } from "react-router-dom";
 
 const ProductDetailPage = () => {
-  const location = useLocation();
   return (
     <>
-      <Breadcrumbs current="Detail" detail={true} />
+      <Breadcrumbs />
       <ProductDetail />;
     </>
   );
