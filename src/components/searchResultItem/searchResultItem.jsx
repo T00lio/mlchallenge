ec1ed775ea0478fc302f3d288b37d4dfb059a622
Tulio Salvatierra <salvacorp@gmail.com>
2023-08-10 21:54:28 -0500

    Display prices in USD

--- src/components/searchResultItem/searchResultItem.jsx
+++ src/components/searchResultItem/searchResultItem.jsx
@@ -4,12 +4,18 @@ import "./searchResultItem.css";
 const SearchResultItem = ({ id, imageUrl, price, title, location }) => {
   return (
     <Container className="productResult">
-      <div className="fi1">
+      <div className="fi1" key={id}>
         <img src={imageUrl} height={100} width={100} alt="Search" />
       </div>
       <div className="fi2">
         <h5>{title}</h5>
-        <p> {price} </p>
+        <p>
+          {" "}
+          {Number(price).toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          })}{" "}
+        </p>
       </div>
       <div className="fi3">
         <h5>{location}</h5>
