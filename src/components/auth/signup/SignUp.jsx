0af8a9868d7311206c1993e88ee1a89efa37787e
Tulio Salvatierra <salvacorp@gmail.com>
2023-09-10 15:55:39 -0500

    fixed signup layout

--- src/components/auth/signup/SignUp.jsx
+++ src/components/auth/signup/SignUp.jsx
@@ -8,6 +8,28 @@ function SignUp() {
   const [password, setPassword] = useState("");
   const [, setError] = useState("");
 
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: "100vh",
+  };
+
+  const formContainerStyle = {
+    width: "100%",
+    maxWidth: "400px",
+    padding: "16px",
+    borderRadius: "8px",
+    backgroundColor: "#ffffff",
+    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+    textAlign: "center",
+  };
+
+  const formTitleStyle = {
+    marginBottom: "16px",
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setError("");
@@ -15,9 +37,11 @@ function SignUp() {
   };
 
   return (
-    <div>
-      <Container component="main" maxWidth="xs">
-        <Typography variant="h5">Create account</Typography>
+    <div style={containerStyle}>
+      <Container component="main" maxWidth="xs" style={formContainerStyle}>
+        <Typography variant="h5" style={formTitleStyle}>
+          Create account
+        </Typography>
         <Typography variant="body2">
           Already have an account? <Link href="/login">Log In</Link>
         </Typography>
